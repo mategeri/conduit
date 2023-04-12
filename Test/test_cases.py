fb9102c58528c5f41c9cf45efef519f8e90c5d53
Máté <mate.gergely89@gmail.com>
2023-04-12 22:01:48 +0200

    test_cases update

--- Test/test_cases.py
+++ Test/test_cases.py
@@ -11,13 +11,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-
 class TestConduit:
-
     username = 'james_bond'
     email = 'james_bond007@gmail.com'
     password = 'James007'
-
 
     def setup_method(self):
         s = Service(executable_path=ChromeDriverManager().install())
@@ -45,9 +42,9 @@ class TestConduit:
         email_input.send_keys(self.email)
         password_input.send_keys(self.password)
         confirm_signin.click()
-        time.sleep(1)
+        time.sleep(3)
 
-# TC1 - Adatkezelési nyilatkozat használata
+    # TC1 - Adatkezelési nyilatkozat használata
 
     def test_cookies(self):
         self.browser.refresh()
@@ -63,7 +60,7 @@ class TestConduit:
 
         print("TC1 - Az adatkezelési nyilatkozat használata teszteset sikeresen lefutott!")
 
-# TC2 - Regisztráció
+    # TC2 - Regisztráció
 
     def test_registration(self):
         signup_button = self.browser.find_element(By.XPATH, '//a[@href="#/register"]')
@@ -86,7 +83,7 @@ class TestConduit:
 
         print("TC2 - A regisztráció teszteset sikeresen lefutott!")
 
-# TC3 - Bejelentkezés
+    # TC3 - Bejelentkezés
 
     def test_login(self):
         self.login()
@@ -97,12 +94,10 @@ class TestConduit:
         assert profile.text == self.username
         print("TC3 - A bejelentkezés teszteset sikeresen lefutott!")
 
-# TC4 - Adatok listázása
+    # TC4 - Adatok listázása
     def test_data_list(self):
         self.login()
-        time.sleep(2)
 
-        # Global Feed
         global_feed_link = self.browser.find_element(By.XPATH, '//a[@href="#/" and @aria-current="page"]')
         global_feed_link.click()
         time.sleep(2)
@@ -115,22 +110,21 @@ class TestConduit:
             print(f"{i}. {title}")
 
         time.sleep(2)
-
         self.browser.refresh()
         time.sleep(2)
 
         refreshed_articles = self.browser.find_elements(By.XPATH,
-                                                    '//div[contains(@class, "home-page")]//div[contains(@class, "article-preview")]//h1')
+                                                        '//div[contains(@class, "home-page")]//div[contains(@class, "article-preview")]//h1')
 
         refreshed_article_titles = [article.text for article in refreshed_articles[:10]]
         assert article_titles == refreshed_article_titles
         print("TC4 - Adatok listázása teszteset sikeresen lefutott!")
 
-# TC5 - Több oldalas lista bejárása
+    # TC5 - Több oldalas lista bejárása
 
     def test_all_pages(self):
         self.login()
-        time.sleep(2)
+
         page_numbers = self.browser.find_elements(By.XPATH, '//a[@class="page-link"]')
         pages = []
         for link in page_numbers:
@@ -140,10 +134,10 @@ class TestConduit:
         assert len(page_numbers) == len(pages)
         print("TC5 - A több oldalas lista bejárása teszteset sikeresen lefutott!")
 
-# TC6 - Új adat bevitel
+    # TC6 - Új adat bevitel
     def test_new_article(self):
         self.login()
-        time.sleep(2)
+
         new_article_button = self.browser.find_element(By.XPATH, '//a[@href="#/editor"]')
         new_article_button.click()
         time.sleep(1)
@@ -169,11 +163,11 @@ class TestConduit:
         assert any(title.text == 'Új adat bevitel teszt title' for title in title_elements)
         print("TC6 - Az új adat bevitel teszteset sikeresen lefutott!")
 
-# TC7 Ismételt és sorozatos adatbevitel adatforrásból
+    # TC7 Ismételt és sorozatos adatbevitel adatforrásból
 
     def test_import_datas_from_csv(self):
         self.login()
-        time.sleep(2)
+
         datas_file = 'datas.csv'
         datas_path = os.path.join(os.path.dirname(__file__), datas_file)
         with open(datas_path, 'r') as file:
@@ -213,11 +207,11 @@ class TestConduit:
                 assert article_title.text == title
                 print("TC7 - Az ismételt és sorozatos adatbevitel adatforrásból teszteset sikeresen lefutott!")
 
-# TC8 Meglévő adat módosítás
+    # TC8 Meglévő adat módosítás
 
     def test_modification_of_existing_username(self):
         self.login()
-        time.sleep(2)
+
         settings_link = self.browser.find_element(By.XPATH, '//a[@href="#/settings"]')
         settings_link.click()
         time.sleep(1)
@@ -235,12 +229,10 @@ class TestConduit:
         assert updated_username == modified_username
         print("TC8 - A meglévő adat módosítás teszteset sikeresen lefutott!")
 
-# TC9 Adat vagy adatok törlése.
+    # TC9 Adat vagy adatok törlése.
 
     def test_delete_data(self):
         self.login()
-
-        time.sleep(2)
 
         new_article_link = self.browser.find_element(By.XPATH, '//a[contains(text(), "New Article")]')
         new_article_link.click()
@@ -278,11 +270,11 @@ class TestConduit:
         assert comment_element is not None
         print("TC9 - Adat vagy adatok törlése teszteset sikeresen lefutott!")
 
-#TC10 Adatok lementése a felületről
+    # TC10 Adatok lementése a felületről
 
     def test_save_data_to_csv(self):
         self.login()
-        time.sleep(2)
+
         tag_list = WebDriverWait(self.browser, 5).until(EC.presence_of_all_elements_located(
             (By.XPATH, '//div[@class="sidebar"]/div/a[@class="tag-pill tag-default"]')))
         csv_file = 'tags.csv'
@@ -298,11 +290,10 @@ class TestConduit:
             assert first_row == tag_list[0].text
             print("TC10 - Adatok lementése a felületről teszteset sikeresen lefutott!")
 
-#     # TC11 Kijelentkezés
-#
+    # TC11 Kijelentkezés
+
     def test_logout(self):
         self.login()
-        time.sleep(2)
 
         logout_button = self.browser.find_element(By.XPATH, '//a[contains(.,"Log out")]')
         logout_button.click()
